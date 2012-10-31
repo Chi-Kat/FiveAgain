24dd1f606b28291b49aa02f1553ef2ac0ad90328
Kathy Li <kathy.j.li@gmail.com>
2012-10-31 02:13:03 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiveAgain::Application.config.secret_token = '51b327c7fd2b356f8ddb5751f88b65b9cbfc9738e4a3e21ee4577d46bed600064ecdd7d8b230ec2b9a4d101d8e45b0bede8143b69d2fc1dc82b441051483cb00'
